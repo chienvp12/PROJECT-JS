ab091b0259749e658a6d1f7b4d83832334b32321
Nguyen Chien <chienvp12@gmai.com>
2022-11-15 10:41:54 +0700

    first commit

---
+++ login/login.js
@@ -0,0 +1,13 @@
+// function check(){
+//     var storeduser = localStorage.getItem('username');
+//     var storedPw = localStorage.getItem('pw');
+
+//     var userName = document.getElementById('userName');
+//     var userPw = document.getElementById('userPw');
+    
+//     if(userName.value == storeduser && userPw.value == storedPw){
+//         alert('You are logged in.');
+//     }else{
+//         alert('Error on login');
+//     }
+// }
