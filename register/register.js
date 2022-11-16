274cc7dd88dfadbbf21b4595e11d1301b149b6e5
C2000 <chienvp12@gmail.com>
2022-11-16 09:37:52 +0700

    the second commit

--- register/register.js
+++ register/register.js
@@ -17,23 +17,14 @@ function Validator(options) {
     function validate(inputElement, rule) {
         var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
         var errorMessage;
-
+        
         // Lấy ra các rules của selector
         var rules = selectorRules[rule.selector];
         
         // Lặp qua từng rule & kiểm tra
         // Nếu có lỗi thì dừng việc kiểm
         for (var i = 0; i < rules.length; ++i) {
-            switch (inputElement.type) {
-                case 'radio':
-                case 'checkbox':
-                    errorMessage = rules[i](
-                        formElement.querySelector(rule.selector + ':checked')
-                    );
-                    break;
-                default:
-                    errorMessage = rules[i](inputElement.value);
-            }
+            errorMessage = rules[i](inputElement.value);
             if (errorMessage) break;
         }
         
@@ -50,11 +41,13 @@ function Validator(options) {
 
     // Lấy element của form cần validate
     var formElement = document.querySelector(options.form);
+    
+    // formElement.addEventListener("submit", )
     if (formElement) {
         // Khi submit form
         formElement.onsubmit = function (e) {
             e.preventDefault();
-
+            
             var isFormValid = true;
 
             // Lặp qua từng rules và validate
@@ -65,43 +58,27 @@ function Validator(options) {
                     isFormValid = false;
                 }
             });
-
             if (isFormValid) {
                 // Trường hợp submit với javascript
                 if (typeof options.onSubmit === 'function') {
+                    
                     var enableInputs = formElement.querySelectorAll('[name]');
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
-                        
-                        switch(input.type) {
-                            case 'radio':
-                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
-                                break;
-                            case 'checkbox':
-                                if (!input.matches(':checked')) {
-                                    values[input.name] = '';
-                                    return values;
-                                }
-                                if (!Array.isArray(values[input.name])) {
-                                    values[input.name] = [];
-                                }
-                                values[input.name].push(input.value);
-                                break;
-                            case 'file':
-                                values[input.name] = input.files;
-                                break;
-                            default:
-                                values[input.name] = input.value;
-                        }
-
+                        values[input.name] = input.value;
                         return values;
                     }, {});
+                    // const user_item = [];
+                    // user_item.push(formValues);
+                    // localStorage.setItem("User", JSON.stringify(user_item));
                     options.onSubmit(formValues);
                 }
                 // Trường hợp submit với hành vi mặc định
                 else {
+                    
                     formElement.submit();
                 }
             }
+            // console.log(enableInputs);
         }
 
         // Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ...)
@@ -133,9 +110,6 @@ function Validator(options) {
     }
 
 }
-
-
-
 // Định nghĩa rules
 // Nguyên tắc của các rules:
 // 1. Khi có lỗi => Trả ra message lỗi
@@ -154,7 +128,7 @@ Validator.isUser = function (selector, message) {
         selector: selector,
         test: function (value) {
             var regex = /^\w+([\.-]?\w+)+$/;
-            return regex.test(value) ? undefined :  message || 'Vui long nhap Username';
+            return regex.test(value) ? undefined :  message || 'Vui lòng nhập username';
         }
     };
 }
@@ -177,6 +151,10 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
     }
 }
 
+
+function addUser(){
+
+}
 function Users(fullname, username, password){
     this.fullname;
     this.username;
